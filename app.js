13aa259946075ed457fadc1c6f7eec16e090a093
aashishrupreja <aashish.rupreja4797@gmail.com>
2023-06-25 18:51:48 +0530

    Reduce calculator size and make text on display wrap

--- app.js
+++ app.js
@@ -2,29 +2,38 @@ const numberKeys = Array.from(document.getElementsByClassName("number-key"));
 const operationKeys = Array.from(document.getElementsByClassName("operation-key"));
 const clearKey = document.getElementById("clear");
 const delKey = document.getElementById("del");
-const display = document.getElementById("display");
+const equalsKey = document.getElementById("equals");
+const displayExpression = document.getElementById("expression");
+const displayAnswer = document.getElementById("answer");
 
 
 clearKey.addEventListener("click", function clearDisplay(e) {
-    if (display.hasChildNodes)
-        display.innerText = "";
+    displayExpression.innerText = "";
 })
 
 delKey.addEventListener("click", function deleteCharacter(e) {
-    let updatedText = display.innerText.slice(0, -1);
-    display.innerText = "";
-    display.append(updatedText);
+    displayExpression.innerText = displayExpression.innerText.slice(0, -1);
 })
+
+equalsKey.addEventListener("click", function (e) {
+    let expression = displayExpression.textContent.split(" ");
+    expressionEvaluator(expression);
+})
+
+function expressionEvaluator(expression) {
+    // displayAnswer.textContent += expression;
+    console.log(expression.length);
+}
 
 for (numberKey of numberKeys) {
     numberKey.addEventListener("click", function putNumberOnDisplay(e) {
-        display.append(this.innerText);
+        displayExpression.textContent += `${this.textContent}`;
     })
 }
 
 for (operationKey of operationKeys) {
     operationKey.addEventListener("click", function putOperatorOnDisplay(e) {
         if (this.innerText !== "=")
-            display.append(this.innerText);
+            displayExpression.textContent += ` ${this.textContent} `;
     })
 }
