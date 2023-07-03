f51be52b0d0585f1c32fd3ddac3942723e34cd59
aashishrupreja <aashish.rupreja4797@gmail.com>
2023-07-03 13:55:51 +0530

    Complete basic functionality

--- app.js
+++ app.js
@@ -3,37 +3,102 @@ const operationKeys = Array.from(document.getElementsByClassName("operation-key"
 const clearKey = document.getElementById("clear");
 const delKey = document.getElementById("del");
 const equalsKey = document.getElementById("equals");
-const displayExpression = document.getElementById("expression");
-const displayAnswer = document.getElementById("answer");
+const previousScreen = document.getElementById("previous");
+const currentScreen = document.getElementById("current");
 
+let num1 = "";
+let operator = "";
+let num2 = "";
+let isExpressionValid = false;
 
 clearKey.addEventListener("click", function clearDisplay(e) {
-    displayExpression.innerText = "";
+    previousScreen.textContent = "";
+    currentScreen.textContent = "";
+    clearAll();
 })
 
 delKey.addEventListener("click", function deleteCharacter(e) {
-    displayExpression.innerText = displayExpression.innerText.slice(0, -1);
+    if (currentScreen.textContent != "")
+        currentScreen.textContent = currentScreen.textContent.slice(0, -1);
+    else {
+        previousScreen.textContent = previousScreen.textContent.slice(0, -1);
+    }
 })
 
 equalsKey.addEventListener("click", function (e) {
-    let expression = displayExpression.textContent.split(" ");
-    expressionEvaluator(expression);
-})
-
-function expressionEvaluator(expression) {
-    // displayAnswer.textContent += expression;
-    console.log(expression.length);
-}
+    setOperandsAndOperatorAsPerDisplay();
+    if (isExpressionValid)
+        currentScreen.textContent = calculate();
+    clearAll();
+});
 
 for (numberKey of numberKeys) {
-    numberKey.addEventListener("click", function putNumberOnDisplay(e) {
-        displayExpression.textContent += `${this.textContent}`;
+    numberKey.addEventListener("click", function (e) {
+        if (currentScreen.textContent.length <= 10) {
+            currentScreen.textContent += this.textContent;
+        }
     })
 }
 
 for (operationKey of operationKeys) {
-    operationKey.addEventListener("click", function putOperatorOnDisplay(e) {
-        if (this.innerText !== "=")
-            displayExpression.textContent += ` ${this.textContent} `;
+    operationKey.addEventListener("click", function (e) {
+
+        operator = this.textContent;
+
+        if (currentScreen.textContent != "") {
+            previousScreen.textContent = currentScreen.textContent;
+            previousScreen.textContent += ` ${operator}`
+            currentScreen.textContent = "";
+        }
     })
+}
+
+function setOperandsAndOperatorAsPerDisplay() {
+
+    const expression = `${previousScreen.textContent} ${currentScreen.textContent}`;
+    const individualValues = expression.split(" ");
+
+    num1 = Number(individualValues[0]);
+    operator = individualValues[1];
+    num2 = Number(currentScreen.textContent);
+
+    if (num1 != null && operator != null && num2 != null)
+        isExpressionValid = true;
+
+    if (previousScreen.textContent.length < 5)
+        previousScreen.textContent = expression;
+
+    console.log(expression);
+    console.log(num1, operator, num2);
+}
+
+function calculate() {
+
+    let answer;
+
+    if (operator == "+") {
+        answer = num1 + num2;
+        return answer;
+    } if (operator == "-") {
+        answer = num1 - num2;
+        return answer;
+    } if (operator == "x") {
+        answer = num1 * num2;
+        return answer;
+    } if (operator == "/") {
+        if (num2 != 0) {
+            answer = num1 / num2;
+            return answer;
+        }
+        else {
+            alert("Can't divide by 0!");
+        }
+    }
+}
+
+function clearAll() {
+    num1 = "";
+    operator = "";
+    num2 = "";
+    isExpressionValid = false;
 }
